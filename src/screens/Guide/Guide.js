64324236a53902f1128818f8e8a1fd4ad2004984
Emilly Soriano <emillysoriano33@gmail.com>
2025-09-28 18:44:30 -0300

    Update Guide.js
    Código da tela de guia de separação

--- src/screens/Guide/Guide.js
+++ src/screens/Guide/Guide.js
@@ -1,21 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Image } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
 import { styles } from './GuideStyle';
-import { Button } from '../../components/Button/Button'
-import { Header } from '../../components/Header/Header'
+import { Header } from '../../components/Header/Header';
 
-
-
-
-export default function Guide({navigation}) {
-    return(
+export default function Guide({ navigation }) {
+  return (
     <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Header 
-          title="Guia de separação"
-        />
-      </View>
+      <Header title="Guia de Separação" />
+      <View style={styles.guideContainer}>
 
+        <ScrollView style={styles.content} contentContainerStyle={styles.contentContainer}>
+          <View style={styles.guideInfo}>
+
+            <View style={[styles.card, styles.cardPapel]}>
+              <View style={styles.cardHeader}>
+                <Image style={styles.cardIcon} source={require('../../../assets/paper.png')} />
+                <Text style={styles.cardTitle}>Papel</Text>
+              </View>
+              <Text style={styles.cardText}>Remova restos e amasse caixas planas.</Text>
+            </View>
+
+            <View style={[styles.card, styles.cardVidro]}>
+              <View style={styles.cardHeader}>
+                <Image style={styles.cardIcon} source={require('../../../assets/glass.png')} />
+                <Text style={styles.cardTitle}>Vidro</Text>
+              </View>
+              <Text style={styles.cardText}>Entregue inteiro, sem quebrar.</Text>
+            </View>
+
+            <View style={[styles.card, styles.cardPlastico]}>
+              <View style={styles.cardHeader}>
+                <Image style={styles.cardIcon} source={require('../../../assets/plastic.png')} />
+                <Text style={styles.cardTitle}>Plástico</Text>
+              </View>
+              <Text style={styles.cardText}>Lave garrafas e amasse sacolas. Não amasse.</Text>
+            </View>
+            
+            <View style={[styles.card, styles.cardMetal]}>
+              <View style={styles.cardHeader}>
+                <Image style={styles.cardIcon} source={require('../../../assets/metal.png')} />
+                <Text style={styles.cardTitle}>Metal</Text>
+              </View>
+              <Text style={styles.cardText}>Lave latas e remova resíduos.</Text>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
     </View>
-    )
+  );
 }
